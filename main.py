e0ffb001da2659f1f7aa0f777d9136310ed6a470
Bubwah <laurens-christian@hotmail.com>
2024-02-04 13:42:14 +0100

    first reworks for code clean-up

--- main.py
+++ main.py
@@ -1,64 +1,65 @@
-import random
-from random import randint
-import time
 import os
-from pynput import keyboard
-from pynput.keyboard import Key
+import random
 import threading
+import time
 from msvcrt import getch
+from random import randint
+
 space = ""
 movement = ""
 fall_list = list(range(18))
-empty_line = ["0", "0","0","0","0","0","0","0","0","0"]
-board = [["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"],
-              ["0", "0","0","0","0","0","0","0","0","0"]]
+empty_line = ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"]
+board_height = 20
+board = [["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+         ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"]]
+
 tetris_shapes = {
-    "Iblockhor":"[][][][]",
-    "Iblockver":"[]\n[]\n[]\n[]",
-    "Jblock1":"[]\n[][][]",
-    "Jblock2":"    []\n\r[][][]",
-    "Jblock3":"[][]\n[]\n[]",
-    "Jblock4":"[][]\n  []\n  []",
-    "Jblock5":"[]\n[]\n[][]",
-    "Jblock6":"  []\n  []\n[][]",
-    "Jblock7":"[][][]\n[]",
-    "Jblock8":"[][][]\n    []",
-    "Oblock":"[][]\n[][]",
-    "Sblock1":"  [][]\n[][]",
-    "Sblock2":"[][]\n  [][]",
-    "Sblock3":"[]\n[][]\n  []",
-    "Sblock4":"  []\n[][]\n[]",
-    "Tblock1":"  []\n[][][]",
-    "Tblock2":"[][][]\n  []",
-    "Tblock3":"[]\n[][]\n[]",
-    "Tblock4":  ["[]" , "\n" ,"[]","[]","\n",  "[]"]
+    "Iblockhor": "[][][][]",
+    "Iblockver": "[]\n[]\n[]\n[]",
+    "Jblock1": "[]\n[][][]",
+    "Jblock2": "    []\n\r[][][]",
+    "Jblock3": "[][]\n[]\n[]",
+    "Jblock4": "[][]\n  []\n  []",
+    "Jblock5": "[]\n[]\n[][]",
+    "Jblock6": "  []\n  []\n[][]",
+    "Jblock7": "[][][]\n[]",
+    "Jblock8": "[][][]\n    []",
+    "Oblock": "[][]\n[][]",
+    "Sblock1": "  [][]\n[][]",
+    "Sblock2": "[][]\n  [][]",
+    "Sblock3": "[]\n[][]\n  []",
+    "Sblock4": "  []\n[][]\n[]",
+    "Tblock1": "  []\n[][][]",
+    "Tblock2": "[][][]\n  []",
+    "Tblock3": "[]\n[][]\n[]",
+    "Tblock4": ["[]", "\n", "[]", "[]", "\n", "[]"]
 }
 lines = [board[0], board[1], board[2], board[3], board[4], board[5], board[6],
          board[7], board[8], board[9], board[10], board[11], board[12], board[13],
          board[14], board[15], board[16], board[17], board[18], board[19]]
 
 
-
-
 def draw_screen():
+    os.system('cls')
+    print("\n\n")
     for row in range(len(board)):
         line = "| "
 
@@ -66,29 +67,16 @@ def draw_screen():
             character = convert_to_symbol(board[row][col])
             line += f" {character} "
 
-
-
         print(line + " |")
-
-
-
-
-
-
-
-
-
-
-
-
+    time.sleep(0.5)
 
 
 def game_loop():
-    #run the main loop of the game, moves the blocks downwards, also keep score.
+    # run the main loop of the game, moves the blocks downwards, also keep score.
     t1 = threading.Thread(target=user_input)
-    t2 = threading.Thread(target=draw_Oblock)
-    Game_Exit = False
-    while not Game_Exit:
+    t2 = threading.Thread(target=draw_square_block)
+    game_exit = False
+    while not game_exit:
 
         # selected_shape = tetris_shape_select()
         selected_shape = "Oblock"
@@ -96,25 +84,27 @@ def game_loop():
             t1.start()
             t2.start()
 
-
-        Game_Exit = True
+        game_exit = True
     t1.join()
     t2.join()
 
-    #return keep_score()
+    # return keep_score()
+
 
 def tetris_shape_select():
     selected_shape = random.choice(list(tetris_shapes.keys()))
     return selected_shape
 
-#def keep_score():
-    #adds 1 to a removed rows counter every time a row is removed and sets the score to that counter
 
-#def write_score():
-    #write the score at the end of the game to a txt file
-    #game_loop()
-def reset_lines(starting_line1):
-    board[starting_line1 - 1] = empty_line
+# def keep_score():
+# adds 1 to a removed rows counter every time a row is removed and sets the score to that counter
+
+# def write_score():
+# write the score at the end of the game to a txt file
+# game_loop()
+def reset_line(line):
+    board[line] = empty_line
+
 
 def clear_screen():
     global space
@@ -125,15 +115,12 @@ def clear_screen():
     return space
 
 
-
-
-
-def user_input() -> none:
+def user_input() -> None:
     global movement
     while True:
         key = ord(getch())
         if key == 27:  # ESC
-            break
+            exit()
         elif key == 32:  # Space
             movement = "rotate"
             print('rotate')
@@ -152,6 +139,7 @@ def user_input() -> none:
                 time.sleep(0.5)
                 movement = ""
 
+
 def convert_to_symbol(number):
     if int(number) == 0:
         return ' * '
@@ -161,68 +149,55 @@ def convert_to_symbol(number):
         return '[ ]'
     return "error"
 
-def draw_Oblock():
+
+def draw_square_block():
+    block_height = 2
     starting_line1 = 0
     starting_line2 = 1
     j = 0
-    start_pos = randint(1,9)
-    for i in range(start_pos -1, start_pos+1):
-        print(start_pos)
+    starting_col = randint(1, 9)
+
+    # Draw initial block
+    for i in range(starting_col - 1, starting_col + 1):
+        print(starting_col)
         lines[starting_line1][i] = "1"
         lines[starting_line2][i] = "1"
 
     draw_screen()
 
-    for i in fall_list:
-        starting_line1 +=1
-        starting_line2 +=1
-        time.sleep(0.5)
-        os.system('cls')
-        draw_screen()
-        if movement == "left" and start_pos > -1:
-            start_pos -= 1
+    # Move the block down step by step
+    for _ in range(board_height-block_height):
+        starting_line1 += 1
+        starting_line2 += 1
 
-        if movement == "right" and start_pos < 9:
-            start_pos += 1
-
-        reset_lines(starting_line1)
-        print("\n")
-        for j in range(start_pos - 1, start_pos +1):
-            print(start_pos)
+        for j in range(starting_col - 1, starting_col + 1):
+            print(starting_col)
             lines[starting_line1][j] = "1"
             lines[starting_line2][j] = "1"
 
+        reset_line(starting_line1 - 1)
+        draw_screen()
+        if movement == "left" and starting_col > -1:
+            starting_col -= 1
+
+        if movement == "right" and starting_col < 9:
+            starting_col += 1
+
+        print("\n")
+
+    # Make the block fixed
     lines[starting_line1][j] = "2"
     lines[starting_line2][j] = "2"
-    clear_screen()
-
-    time.sleep(0.5)
-    os.system('cls')
-    time.sleep(0.5)
-    draw_screen()
-
-
-
-
-
-
+    # clear_screen()
 
 
 def main():
-    #draw_screen()
-    #write_score()
-
+    # draw_screen()
+    # write_score()
 
     game_loop()
-
-
-
-
-
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-
